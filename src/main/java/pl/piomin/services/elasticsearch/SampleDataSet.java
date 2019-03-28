6d9ffe5012f3db7f84678d70057ed33a5ed45eba
minkowp <piotr.minkowski@play.pl>
2019-03-28 15:50:36 +0100

    changing bulk

--- src/main/java/pl/piomin/services/elasticsearch/SampleDataSet.java
+++ src/main/java/pl/piomin/services/elasticsearch/SampleDataSet.java
@@ -29,7 +29,7 @@ public class SampleDataSet {
 
     @PostConstruct
     public void init() {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10000; i++) {
             bulk(i);
         }
     }
