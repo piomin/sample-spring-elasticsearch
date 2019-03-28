dd7331c6e0199fa4176178529063816d5cfbe588
minkowp <piotr.minkowski@play.pl>
2019-03-28 15:38:19 +0100

    fixing test name

--- src/test/java/pl/piomin/services/elasticsearch/EmployeeRepositoryTest.java
+++ src/test/java/pl/piomin/services/elasticsearch/EmployeeRepositoryTest.java
@@ -2,10 +2,8 @@ package pl.piomin.services.elasticsearch;
 
 import org.junit.*;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
@@ -15,13 +13,11 @@ import pl.piomin.services.elasticsearch.model.Organization;
 import pl.piomin.services.elasticsearch.repository.EmployeeRepository;
 
 import java.util.List;
-import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OrganizationRepositoryTest {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrganizationRepositoryTest.class);
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EmployeeRepositoryTest {
 
     @ClassRule
     public static ElasticsearchContainer container = new ElasticsearchContainer();
@@ -30,17 +26,15 @@ public class OrganizationRepositoryTest {
 
     @BeforeClass
     public static void before() {
-        System.setProperty("spring.data.elasticsearch.cluster-nodes", "192.168.99.100:" + container.getMappedPort(9300));
+        System.setProperty("spring.data.elasticsearch.cluster-nodes", container.getContainerIpAddress() + ":" + container.getMappedPort(9300));
     }
 
     @Test
     public void testAdd() {
-        Random r = new Random();
-        Long id = repository.count();
         Employee employee = new Employee();
-        employee.setId(id);
-        employee.setName("John Smith" + r.nextInt(1000));
-        employee.setAge(r.nextInt(100));
+        employee.setId(1L);
+        employee.setName("John Smith");
+        employee.setAge(33);
         employee.setPosition("Developer");
         employee.setDepartment(new Department(1L, "TestD"));
         employee.setOrganization(new Organization(1L, "TestO", "Test Street No. 1"));
@@ -51,7 +45,7 @@ public class OrganizationRepositoryTest {
     @Test
     public void testFindAll() {
         Iterable<Employee> employees = repository.findAll();
-        employees.forEach(it -> LOGGER.info(it.toString()));
+        Assert.assertTrue(employees.iterator().hasNext());
     }
 
     @Test
@@ -62,8 +56,8 @@ public class OrganizationRepositoryTest {
 
     @Test
     public void testFindByName() {
-        List<Employee> employees = repository.findByName("John Smith10000");
-        employees.forEach(it -> LOGGER.info(it.toString()));
+        List<Employee> employees = repository.findByName("John Smith");
+        Assert.assertTrue(employees.size() > 0);
     }
 
 }
