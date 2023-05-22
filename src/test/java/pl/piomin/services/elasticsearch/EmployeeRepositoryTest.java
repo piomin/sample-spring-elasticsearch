6d2d0c5d8fe0a6cf19b56d1a198daecb6fa44544
piomin <piotr.minkowski@gmail.com>
2023-05-23 01:29:18 +0200

    fix es uri

--- src/test/java/pl/piomin/services/elasticsearch/EmployeeRepositoryTest.java
+++ src/test/java/pl/piomin/services/elasticsearch/EmployeeRepositoryTest.java
@@ -33,8 +33,9 @@ public class EmployeeRepositoryTest {
 
     @DynamicPropertySource
     static void registerElasticsearchProperties(DynamicPropertyRegistry registry) {
-        String uri = container.getContainerIpAddress() + ":" + container.getMappedPort(9300);
-        registry.add("spring.data.elasticsearch.cluster-nodes", () -> uri);
+        String uri = "http://" + container.getContainerIpAddress() + ":" + container.getMappedPort(9300);
+        registry.add("spring.elasticsearch.uris", () -> uri);
+        registry.add("spring.elasticsearch.rest.uris", () -> uri);
     }
 
     @Test
