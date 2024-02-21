ffadf397d995c80786f71bc16330a95b8229e105
piomin <piotr.minkowski@gmail.com>
2024-02-21 15:40:30 +0100

    fix sonar (3)

--- employee-reactive-service/src/main/java/elasticsearch/SampleApplication.java
+++ employee-reactive-service/src/main/java/elasticsearch/SampleApplication.java
@@ -1,12 +1,9 @@
 package elasticsearch;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SampleApplication {
 
     public static void main(String[] args) {
-        SampleApplication.run(SampleApplication.class, args);
+
     }
 
 }
